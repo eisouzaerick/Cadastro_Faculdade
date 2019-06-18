d14b3da93d0fe766e829639604b4c2f22738e2d0
Davidson Eustaquio <davideustaquio@gmail.com>
2019-06-18 20:35:17 -0300

    Classes Aluno e Professor
    .

---
+++ Aluno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrinCadastroPROFAlun
+{
+    class Aluno: Pessoa
+    {
+        private int matricula;
+
+        public int Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        override public string MostraPessoa()
+        {
+            string aux = "";
+            aux = base.MostraPessoa() + "\nMatricula: " + matricula.ToString() + "\n____________";
+
+            return aux;
+        }
+    }
+}
---
+++ Professor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrinCadastroPROFAlun
+{
+    class Professor: Pessoa
+    {
+        private string graduação;
+        private List<Aluno> aluno;
+
+        public string Graduação
+        {
+            get { return graduação; }
+            set { graduação = value; }
+        }
+
+        public Professor()
+        {
+            Alunos = new List<Aluno>();
+        }
+
+        public List<Aluno> Alunos
+        {
+            get { return aluno; }
+            set { aluno = value; }
+        }
+
+        public void AddAlunos(Aluno A)
+        {
+            Alunos.Add(A);
+        }
+
+        override public string MostraPessoa()
+        {
+            string aux = "";
+            aux = base.MostraPessoa() + "\nGraduação: " + graduação;
+
+            return aux;
+        }
+    }
+}
