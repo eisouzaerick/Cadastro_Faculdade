8bec039b16458e443756789551e523e578b4fd18
Erick Souza Pereira da Silva <eisouzaerick@outlook.com>
2019-06-18 20:32:00 -0300

    Class Pessoa
    Classe pessoa já finalizada com adição de nome

---
+++ Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrinCadastroPROFAlun
+{
+    class Pessoa
+    {
+        private string nome;
+        private int ano;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public int Ano
+        {
+            get { return ano; }
+            set { ano = value; }
+        }
+
+        virtual public string MostraPessoa()
+        {
+            return "Nome: "+Nome+ "\nPeriodo: " + Ano.ToString();
+        }
+    }
+}
